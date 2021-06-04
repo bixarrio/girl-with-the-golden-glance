c232a01b63fa387532cd6fe366031b859fcf268d
Corne van Rensburg <cornevr@bbd.co.za>
2021-06-04 22:25:14 +0200

    Fixed recurring cutscene

--- Assets/Scripts/Special/Start Cutscene/SignalHandler.cs
+++ Assets/Scripts/Special/Start Cutscene/SignalHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class SignalHandler : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class SignalHandler : MonoBehaviour
 
     [SerializeField] GameObject[] _uiShot;
     [SerializeField] Transform[] _position;
+    [SerializeField] PlayableDirector _director;
 
     #endregion
 
@@ -26,6 +28,18 @@ public class SignalHandler : MonoBehaviour
     public void MoveToPosition(int idx)
     {
         CharacterMovementController.Instance.SetDestination(_position[idx].position);
+    }
+
+    public void StopTimelineIfPlayed()
+    {
+        if (GameEventController.Instance.GameEventIsSet("Opening Scene Played"))
+        {
+            _director.Stop();
+        }
+        else
+        {
+            Messaging<GameEvent>.Trigger?.Invoke("Opening Scene Played", true);
+        }
     }
 
     public void SaySomething(string narrative) => Messaging<ShowNarrative>.Trigger?.Invoke(narrative);
