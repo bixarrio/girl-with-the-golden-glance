5d4d5725d4f6a3c0bdb56ee5f40c2c8a18ae4ea0
Corne van Rensburg <cornevr@bbd.co.za>
2021-06-06 01:42:41 +0200

    Fixes: - Cleaned up text on Audio screen - Added tooltips to items in inventory - Changed start cutscene to allow control sooner - Tweaked flashback cutscene to be more clearly a flashback

--- Assets/Scripts/UI/Button Handlers/AudioAdjustmentButtons.cs
+++ Assets/Scripts/UI/Button Handlers/AudioAdjustmentButtons.cs
@@ -6,6 +6,23 @@ using UnityEngine.UI;
 
 public class AudioAdjustmentButtons : MonoBehaviour
 {
+    #region Properties and Fields
+
+    [SerializeField] Text _descriptionText;
+    [SerializeField] Text _valueText;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        _descriptionText.text = string.Empty;
+        _valueText.text = string.Empty;
+    }
+
+    #endregion
+
     #region Public Methods
 
     public void DoneClicked() => SceneTransition.Instance.DoTransition("Menu", null, TransitionType.Fade);
